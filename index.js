b22b5121c6d07856b5d4364eee3e39f5d14c2fd8
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2024-01-12 01:02:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
-const express = require("express");
-const cors = require("cors");
-const xss = require("xss-clean");
-const createError = require("http-errors");
-const mainRouter = require("./routes/index");
-const PORT = process.env.PORT || 3000;
+const app = require("./src/express/app");
 require("dotenv").config();
-const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use(xss());
-app.use(express.static("public"));
-
-app.use("/", mainRouter);
-app.all("*", (req, res, next) => {
-  next(createError.NotFound());
-});
-
-app.use((err, req, res, next) => {
-  res.status(err.status || 500).json({
-    message: err.message,
-  });
-});
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
