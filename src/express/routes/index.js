a97c204fd677dd4ef6b4d2c986b4fa56bad012ae
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2024-01-13 02:39:47 +0700

    Update index.js

--- src/express/routes/index.js
+++ src/express/routes/index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const usersRouter = require("./users");
+const recipesRouter = require("./recipes");
+const commentsRouter = require("./comments");
+const likedRouter = require("./liked");
+const savedRouter = require("./saved");
 
 router.use("/users", usersRouter);
+router.use("/recipes", recipesRouter);
+router.use("/comments", commentsRouter);
+router.use("/liked", likedRouter);
+router.use("/saved", savedRouter);
 
 module.exports = router;
