85d838731cfb1123ec5e7ad4e757443cf97dcf07
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2024-01-25 02:38:44 +0700

    Update app.js

--- src/express/app.js
+++ src/express/app.js
@@ -13,24 +13,28 @@ app.use(bodyParser.json());
 app.use(morgan("dev"));
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
-const corsOptions = {
-  origin: process.env.CLIENT_URL_DEV || process.env.CLIENT_URL_PROD || "http://localhost:3000",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(xss());
 app.use("/", routes);
+app.use("img", express.static("src/upload"));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Request-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 app.all("*", (req, res, next) => {
-  next(createError.NotFound());
+  next(new createError.NotFound());
 });
 
 app.use((err, req, res, next) => {
-  res.status(err.status || 500).json({
-    message: err.message,
+  const messError = err.message || "Internal Server Error";
+  const statusCode = err.status || 500;
+
+  res.status(statusCode).json({
+    message: messError,
   });
 });
 
