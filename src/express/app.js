8244b802f8172cd2d520411e907a4433fb323c42
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2024-01-25 02:03:44 +0700

    Update app.js

--- src/express/app.js
+++ src/express/app.js
@@ -14,7 +14,7 @@ app.use(morgan("dev"));
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
-  origin: process.env.CLIENT_URL || "http://localhost:3000",
+  origin: process.env.CLIENT_URL_DEV || process.env.CLIENT_URL_PROD,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionsSuccessStatus: 204,
